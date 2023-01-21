ccbd662d8eee7c5e2a25a568ef609a35460db864
Kaveri Mittal <kaverimittal@Shivams-MacBook-Air.local>
2023-01-21 17:15:03 -0500

    fixed the displaying of latest comments on top by array sorting method, created like and delete function for liking and deleting of comments also tested it also addes some styles for like and delete buttons

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,15 +1,12 @@
 let commentArray = [];
-console.log(commentArray);
+// console.log(commentArray);
 
 function getCommentData () {
     axios
     .get(`https://project-1-api.herokuapp.com/comments/?api_key=${apikey}`)
     .then((response) => {
-        const holdingData = response.data;
-        holdingData.forEach(element => {
-        commentArray.push(element);
-        // commentArray.reverse();
-    });
+        commentArray = response.data;
+        commentArray.sort((a,b) =>  new Date(b.timestamp) - new Date(a.timestamp));
         displayComments();
     })
     .catch((error) => {
@@ -19,7 +16,6 @@ function getCommentData () {
 getCommentData();
 
 function createCommentCard(post){
-    
     const cardEl = document.createElement("div");
     cardEl.classList.add("comments", "comments--border");
 
@@ -35,11 +31,9 @@ function createCommentCard(post){
     const dateEl = document.createElement("p");
     dateEl.classList.add("comments__content--date");
     
-    
     const formattedDate = new Date(post.timestamp);
     const newDate = `${formattedDate.getMonth()+1}/${formattedDate.getDate()}/${formattedDate.getFullYear()}`;
-    dateEl.innerText = newDate;
-    
+    dateEl.innerText = newDate; 
     
     const nameEl = document.createElement("p");
     nameEl.classList.add("comments__content--person");
@@ -58,16 +52,24 @@ function createCommentCard(post){
 
     const likeButtonEL = document.createElement("button");
     likeButtonEL.classList.add("comments__btn-like");
-    likeButtonEL.setAttribute('id', 'like')
+    likeButtonEL.setAttribute('type', 'button')
+    likeButtonEL.setAttribute('value', 'click')
+    likeButtonEL.setAttribute('id', post.id);
+    likeButtonEL.addEventListener('click', likeHandler);
+
+    const likeCountEl = document.createElement('p');
+    likeCountEl.classList.add("comments__like-count");
+    likeCountEl.innerText = post.likes
 
     const deleteButtonEL = document.createElement("button");
     deleteButtonEL.classList.add("comments__btn-delete");
-    deleteButtonEL.setAttribute('id', 'comment-id')
-    
-    commentButtonsFunctionalityEl.append(likeButtonEL, deleteButtonEL);
+    deleteButtonEL.setAttribute('type', 'button')
+    deleteButtonEL.setAttribute('id', post.id)
+    deleteButtonEL.addEventListener('click', deleteHandler);
+
+    commentButtonsFunctionalityEl.append(likeButtonEL, likeCountEl, deleteButtonEL);
 
     commentTextEl.append(commentEl, commentButtonsFunctionalityEl);
-
 
     commentMainContentEl.append(commentContentEl, commentTextEl);
 
@@ -84,56 +86,78 @@ function displayComments() {
     for (let i = 0; i < commentArray.length; i++) {
         myCommentsEl.appendChild(createCommentCard(commentArray[i]));
     }
-
-    // for (let i = commentArray.length-1; i >= 0; i--) {
-    //     myCommentsEl.appendChild(createCommentCard(commentArray[i]));
-    // }
-}
+};
 
 const commentsFormEl = document.querySelector("#form");
 
 function formSubmitHandler(e) {
     e.preventDefault();
 
-    let name = document.getElementById('name').value;
-    let comment = document.getElementById('comment').value;
-    let currentDate = new Date();
-    // let commentData = {
-    //     name: document.getElementById('name').value,
-    //     comment: document.getElementById('comment').value,
-    //     timestamp: `${currentDate.getSeconds()}` + " " + 'seconds ago',
-    // }
-
+    let commentData = {
+        name: e.target.name.value,
+        comment: e.target.comment.value,
+    }
+// this is for post request
     axios
     .post(`https://project-1-api.herokuapp.com/comments/?api_key=${apikey}`, {
-        name: name,
-        comment: comment,
-        // timestamp: `${currentDate.getSeconds()}` + " " + 'seconds ago',
+        name: commentData.name,
+        comment: commentData.comment,
     })
-    .then((response) => {
-        
-        // this is added for displaying comments on top
-        commentArray.push(response.data);
-        // commentArray.sort();
-        // commentArray.reverse();
-        commentArray.unshift(response.data);
-        
-        
-    })
-    .then((res) => {
-        displayComments();
+    .then(() => {
+        getCommentData();
     })
     .catch((error) => {
         console.log(error);
     });
 
     commentsFormEl.reset();
-    displayComments();
 }
 
 commentsFormEl.addEventListener("submit", formSubmitHandler);
-displayComments();
-// getCommentData();
+
+// this function is to like the comment
+function likeHandler (e) {
+    e.preventDefault();
+    
+    let likeId = e.target.id;
+
+    const incrementLike = () => {
+        axios 
+    .put(`https://project-1-api.herokuapp.com/comments/${likeId}/like?api_key=${apikey}`) 
+    .then((response) => {
+        getCommentData();
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+    };
+    incrementLike();
+}
+
+// this function is to delete the comment
+function deleteHandler (e) {
+    e.preventDefault();
+    
+    let deleteId = e.target.id;
+
+    const deleteComment = () => {
+        axios 
+    .delete(`https://project-1-api.herokuapp.com/comments/${deleteId}/?api_key=${apikey}`) 
+    .then((response) => {
+        getCommentData();
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+    };
+    deleteComment();
+}
+
+
+
+
+
+
 
 
 
